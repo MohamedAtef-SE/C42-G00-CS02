7f0dd3b02a90a6a9e4ba556a3058dc8fba5977e8
mohamedatef-SE <mohamedatefyoussef@gmail.com>
2024-06-21 20:58:28 +0300

    01.Write a program that allows the user to enter a number then print it.

---
+++ Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            #region 01.Write a program that allows the user to enter a number then print it.
+
+            //Console.Write("Enter your number: ");
+            //bool flag = int.TryParse(Console.ReadLine(), out int result);
+
+            //if (flag)
+            //{
+            //    Console.WriteLine(result);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Inavalid Data");
+            //}
+            #endregion
+
+
+        }
+    }
+}
